1dccedfe6ed2b5321d5b1281348a808b60e343b2
Isaac Jordan kimsey <jordankimsey@gmail.com>
2023-03-07 11:27:26 -0500

    removed headers

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ if (process.env.NODE_ENV !== 'production') {
 const app = express();
 
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Orgin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Allow-Headers', 'Content-Type, Authorization');
-  next()
-})
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Orgin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+//   res.setHeader('Access-Control-Allow-Allow-Headers', 'Content-Type, Authorization');
+//   next()
+// })
 
 app.use(todoRoutes);
 //app.use(error controller get 404)
